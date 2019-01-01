ebe934521b82fb99e179d3c65fd5a42380b53283
Frank Bouwens <frankkie12345@gmail.com>
2019-01-01 14:55:16 +0100

    WIP Change nick ui

--- src/components/ChangeNickname.js
+++ src/components/ChangeNickname.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 import * as firebase from "firebase";
 import CircularProgress from "@material-ui/core/es/CircularProgress/CircularProgress";
+import TextField from "@material-ui/core/TextField";
+import {withStyles} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/es/Typography/Typography";
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  submit: {
+    margin: theme.spacing.unit,
+    width: 200,
+  }
+});
 
 class ChangeNickname extends Component {
 
@@ -10,12 +26,32 @@ class ChangeNickname extends Component {
   }
 
   render() {
+    const {classes} = this.props;
+
     return (
       <div className='nickname'>
-        Pick a nickname
-        <form onSubmit={this.handleChangeNickname.bind(this)}>
-          <input type='text' ref='nickname' value={this.props.tnq.user.nickname}/>
-          <input type='submit' value='ok' disabled={this.state.showLoading}/>
+        <Typography component="h1" variant="h5">
+          Pick a nickname
+        </Typography>
+        <form onSubmit={this.handleChangeNickname.bind(this)} className={classes.form}>
+          <br />
+          <TextField
+            required
+            id="nickname"
+            label="Nickname"
+            className={classes.textField}
+          />
+          <br/><br/>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            className={classes.submit}
+            onClick="this.form.submit();"
+            disabled={this.state.showLoading}
+          >
+            OK
+          </Button>
           <br/><br/>
           {(this.state.showLoading) ? <CircularProgress/> : ''}
         </form>
@@ -27,7 +63,7 @@ class ChangeNickname extends Component {
     e.preventDefault();
     this.setState({showLoading: true});
     let changeNick = firebase.functions().httpsCallable('changeNickname');
-    changeNick({nickname: this.refs.nickname.value})
+    changeNick({nickname: document.getElementById('nickname').value})
       .then(function (result) {
         console.log("changed nickname");
         this.setState({showLoading: false});
@@ -39,4 +75,4 @@ class ChangeNickname extends Component {
   }
 }
 
-export default ChangeNickname;
+export default withStyles(styles)(ChangeNickname);
