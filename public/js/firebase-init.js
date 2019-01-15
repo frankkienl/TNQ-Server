9d542118a59756a0a754eb4e6b6aaea87d0404b8
Frank Bouwens <frank.bouwens@bitfactory.nl>
2019-01-15 10:09:37 +0100

    Update firebase-init.js

---
+++ public/js/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "***REMOVED***",
+  authDomain: "***.firebaseapp.com",
+  databaseURL: "https://***.firebaseio.com",
+  projectId: "***",
+  storageBucket: "***.appspot.com",
+  messagingSenderId: "***REMOVED***"
+};
+firebase.initializeApp(config);
